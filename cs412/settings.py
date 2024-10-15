8e27b57d251af24b4a64b36e196a5773a3e9965d
Paul Martin Enano <paul.m.enano@gmail.com>
2024-10-15 00:03:46 -0400

    changing url

--- cs412/settings.py
+++ cs412/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = "cs412.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
